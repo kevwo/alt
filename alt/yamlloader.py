43ccf431d3c71976b543239e0cfc6ee7fea567b9
Kevin Woodmansee <kevinwoodmansee@gmail.com>
2015-09-22 23:25:17 -0400

    Added ConfigFactories

---
+++ alt/yamlloader.py
@@ -0,0 +1,35 @@
+import yaml
+import os
+
+
+class AbsoluteConfigFactory(object):
+    def __init__(self, *secret_files, keep_secrets=False):
+        data = ""
+        for file in secret_files:
+            with open(file, 'r') as fin:
+                    data += fin.read() + '\n'
+        self.secrets = data
+        self.secret_keys = [] if keep_secrets else yaml.load(data).keys()
+
+    def get(self, file):
+        if not hasattr(self, file):
+            with open(file, 'r') as fin:
+                data = fin.read()
+            decoded_yaml = yaml.load(self.secrets + data)
+            for key in self.secret_keys:
+                decoded_yaml.pop(key, None)
+            setattr(self, file, decoded_yaml)
+        return getattr(self, file)
+
+class ConfigFactory(AbsoluteConfigFactory):
+    def __init__(self, basedir, *secret_files, keep_secrets=False):
+        self.basedir = basedir
+        secret_files = [self.format_path(file) for file in secret_files]
+        super().__init__(*secret_files, keep_secrets=keep_secrets)
+
+    def get(self, file):
+        file = self.format_path(file)
+        return super().get(file)
+
+    def format_path(self, file):
+        return os.path.join(self.basedir, file if os.path.splitext(file)[1] is not '' else os.path.splitext(file)[0] + ".yaml")
